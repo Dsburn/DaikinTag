08d2eea88fe6038b063b452abfc783f93e4822cd
Dsburn <mpdaus@gmail.com>
2017-12-06 17:47:11 +0800

    add form

--- src/app/pages/qrscan-page/qrscan-page.component.ts
+++ src/app/pages/qrscan-page/qrscan-page.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface User {
+  name: string; // required with minimum 5 characters
+  address: {
+      street?: string; // required
+      postcode?: string;
+  }
+}
+
 @Component({
   selector: 'app-qrscan-page',
   templateUrl: './qrscan-page.component.html',
@@ -9,7 +17,20 @@ export class QrscanPageComponent implements OnInit {
 
   constructor() { }
 
+  public user: User;
+
   ngOnInit() {
+    this.user = {
+      name: '',
+        address: {
+         street: '',
+         postcode: '8000'
+      }
+    };
+  }
+
+  save(model: User, isValid: boolean) {
+    console.log(model, isValid);
   }
 
 }
--- src/app/pages/qrscan-page/qrscan-page.module.ts
+++ src/app/pages/qrscan-page/qrscan-page.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { QRScanRoutes } from '../../routing';
 import { QrscanPageComponent } from './qrscan-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(QRScanRoutes)
+    RouterModule.forChild(QRScanRoutes),
+    FormsModule
   ],
   declarations: [QrscanPageComponent],
   bootstrap: [QrscanPageComponent]
